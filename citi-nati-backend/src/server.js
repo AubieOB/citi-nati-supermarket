6c6020af506df65b3ec040df0b66979f40e75e04
renewableenergyh-a11y <renewableenergyh@gmail.com>
2026-03-09 19:33:46 +0200

    fix: add back PrismaClient instantiation in server.js after revert

--- citi-nati-backend/src/server.js
+++ citi-nati-backend/src/server.js
@@ -20,6 +20,8 @@ const driversOrdersRoutes = require('./routes/drivers.orders.routes');
 const salesRoutes = require('./routes/sales.routes');
 const supportRoutes = require('./routes/support.routes');
 
+const prisma = new PrismaClient();
+
 async function start() {
   try {
     // Ensure upload directories exist
