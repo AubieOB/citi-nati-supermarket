56a41fb4766b7dc451e78b0bf2ca2457438bb460
AubieOB <aubreymkhulanabanda@gmail.com>
2026-04-17 14:10:27 +0200

    fix: increase inbox top offset to 5rem

--- citi-nati-frontend/src/components/admin/AdminInbox.jsx
+++ citi-nati-frontend/src/components/admin/AdminInbox.jsx
@@ -810,7 +810,7 @@ const AdminInbox = () => {
           flexDirection: 'column',
           gap: '1rem',
           overflowY: 'auto',
-          marginTop: '2rem',
+          marginTop: '5rem',
           paddingTop: '0.25rem',
           paddingBottom: '0.5rem',
         }}>
